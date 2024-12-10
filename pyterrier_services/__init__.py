24a5e7f97aec3c1f719c80ea47a9f358388bc386
Sean MacAvaney <sean.macavaney@gmail.com>
2024-12-10 03:22:49 +0300

    name

--- pyterrier_services/__init__.py
+++ pyterrier_services/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.1.0'
 
 from .core import http_error_retry, paginated_search, multi_query
-from .semantic_scholar import SemanticScholar, SemanticScholarRetriever
+from .semantic_scholar import SemanticScholarApi, SemanticScholarRetriever
 
-__all__ = ['http_error_retry', 'paginated_search', 'multi_query', 'SemanticScholar', 'SemanticScholarRetriever']
+__all__ = ['http_error_retry', 'paginated_search', 'multi_query', 'SemanticScholarApi', 'SemanticScholarRetriever']
